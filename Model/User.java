0f8c0b436d95cabfd177532f5c2d4d4952408776
beccabanks <rebeccabanks916@gmail.com>
2025-11-16 18:00:09 -0500

    Update User.java
    Still need toString

--- Model/User.java
+++ Model/User.java
@@ -1,5 +1,41 @@
 package Model;
 
 public class User {
+  private String userID;
+  private String name;
+  private String phone;
+  private String email;
+
+  public User(String userID, String name, String phone, email) {
+    this.userID = userID;
+    this.name = name;
+    this.phone = phone;
+    this.email = email;
+  }
+
+  //Getters & Setters
+
+  public String getUserID() {
+    return userID;  }
+  public void setUserID(String userID) {
+    this.userID = userID;  }
+
+  public String getName() {
+    return name;  }
+  public void setName(String name) {
+    this.name = name;  }
+
+  public String getPhone() {
+    return phone;  }
+  public void setPhone(String phone) {
+    this.phone = phone;
+
+    public String getEmail() {
+      return email;  }
+    public void setEmail(String email) {
+      this.email = email;  }
+      
+  
 
 }
+
