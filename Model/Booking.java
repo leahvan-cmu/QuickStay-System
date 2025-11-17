cfaebcd1570c22c5ba2a8b9619dbc416cb16b1a7
leahvan-cmu <leah.vanneste@gmail.com>
2025-11-17 14:42:38 -0500

    Implemented Booking class

--- Model/Booking.java
+++ Model/Booking.java
@@ -1,5 +1,14 @@
 package Model;
 
 public class Booking {
+    private User user;
+    private Property property;
+
+    public Booking(User user, Property property) {
+        this.user = user;
+        this.property = property;
+    }
+
+    // Getters & Setters
 
 }
