06ed5493360bec2a92ed9889357687de3949a0a4
leahvan-cmu <leah.vanneste@gmail.com>
2025-11-17 18:55:44 -0500

    Finished Booking Class

--- Model/Booking.java
+++ Model/Booking.java
@@ -3,12 +3,47 @@ package Model;
 public class Booking {
     private User user;
     private Property property;
+    private int daysBooked;
+    private double totalPrice;
 
-    public Booking(User user, Property property) {
-        this.user = user;
+    public Booking(Property property, User user, int days) {
+        if (!property.isAvailable()) {
+            throw new IllegalStateException("Property is not available!");
+        }
+        if (days <= 0) {
+            throw new IllegalArgumentException("Days must be greater than 0");
+        }
+
         this.property = property;
+        this.user = user;
+        this.daysBooked = days;
+        this.totalPrice = property.getPrice() * days;
+
+        property.setAvailable(false);
     }
 
-    // Getters & Setters
+    public User getUser() {
+        return user;
+    }
 
+    public Property getProperty() {
+        return property;
+    }
+
+    public int getDays() {
+        return daysBooked;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    // toString method
+    public String toString() {
+        return "Booking Details:" +
+                "\nUserID: " + user.getUserID() +
+                "\nProperty: " + property.getName() +
+                "\nDays: " + daysBooked +
+                "\nTotal Cost: $" + totalPrice;
+    }
 }
