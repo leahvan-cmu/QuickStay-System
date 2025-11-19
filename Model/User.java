a4debc878411c53235d4c76d33f220a1680ab9df
beccabanks <rebeccabanks916@gmail.com>
2025-11-19 17:43:05 -0500

    Added password

--- Model/User.java
+++ Model/User.java
@@ -5,46 +5,41 @@ public class User {
   private String name;
   private String phone;
   private String email;
+  private String password;
 
-  public User(String userID, String name, String phone, String email) {
+  public User(String userID, String name, String phone, String email, String password) {
     this.userID = userID;
     this.name = name;
     this.phone = phone;
     this.email = email;
+    this.password = password;
   }
 
   // Getters & Setters
 
   public String getUserID() {
-    return userID;
-  }
-
+    return userID;  }
   public void setUserID(String userID) {
-    this.userID = userID;
-  }
+    this.userID = userID;  }
 
   public String getName() {
-    return name;
-  }
-
+    return name;  }
   public void setName(String name) {
-    this.name = name;
-  }
+    this.name = name;  }
 
   public String getPhone() {
-    return phone;
-  }
-
+    return phone;  }
   public void setPhone(String phone) {
-    this.phone = phone;
-  }
+    this.phone = phone;  }
 
   public String getEmail() {
-    return email;
-  }
-
+    return email;  }
   public void setEmail(String email) {
-    this.email = email;
-  }
+    this.email = email;  }
+
+  public String getPassword() {
+    return password;  }
+  public void setPassword(String password) {
+    this.password;  }
 
 }
