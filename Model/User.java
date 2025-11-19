771570527810d1899aaee0513706df2db56afd66
beccabanks <rebeccabanks916@gmail.com>
2025-11-19 17:44:56 -0500

    Update User.java

--- Model/User.java
+++ Model/User.java
@@ -1,45 +1,39 @@
 package Model;
 
 public class User {
-  private String userID;
-  private String name;
-  private String phone;
+  private String username;
   private String email;
+  private String currency;
   private String password;
 
-  public User(String userID, String name, String phone, String email, String password) {
-    this.userID = userID;
-    this.name = name;
-    this.phone = phone;
-    this.email = email;
-    this.password = password;
-  }
 
-  // Getters & Setters
+ //Constructor
+  public User(String username, String email, String currency, String password) {
+	this.username = username;
+	this.email = email;
+	this.currency = currency;
+	this.password = password;
+}
 
-  public String getUserID() {
-    return userID;  }
-  public void setUserID(String userID) {
-    this.userID = userID;  }
-
+  //Getters & Setters
   public String getName() {
-    return name;  }
-  public void setName(String name) {
-    this.name = name;  }
-
-  public String getPhone() {
-    return phone;  }
-  public void setPhone(String phone) {
-    this.phone = phone;  }
+	  return username;  }
+  public void setName(String username) {
+  	this.username = username;  }
 
   public String getEmail() {
-    return email;  }
+  	return email;  }
   public void setEmail(String email) {
-    this.email = email;  }
+  	this.email = email;  }
+
+  public String getCurrency() {
+	  return currency;  }
+  public void setCurrency(String currency) {
+	  this.currency = currency; }
 
   public String getPassword() {
-    return password;  }
+  	return password;  }
   public void setPassword(String password) {
-    this.password;  }
+    this.password = password;  }
 
 }
