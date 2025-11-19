5f31770f6651cf7d51d34586e1570ea01e9c33af
leahvan-cmu <leah.vanneste@gmail.com>
2025-11-19 17:19:57 -0500

    Created Property Service class, with addProperty, getAllProperties and getAvailableProperties methods

--- Service/PropertyService.java
+++ Service/PropertyService.java
@@ -1,5 +1,38 @@
 package Service;
 
+import Model.Property;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PropertyService {
+
+    private List<Property> properties = new ArrayList<>();
+
+    /*
+     * Adds a new property to the system.
+     */
+    public void addProperty(Property property) {
+        properties.add(property);
+    }
+
+    /*
+     * Returns a list of all properties.
+     */
+    public List<Property> getAllProperties() {
+        return properties;
+    }
+
+    /*
+     * Returns a list of available properties.
+     */
+    public List<Property> getAvailableProperties() {
+        List<Property> availableProperties = new ArrayList<>();
+        for (Property property : properties) {
+            if (property.isAvailable()) {
+                availableProperties.add(property);
+            }
+        }
+        return availableProperties;
+    }
 
 }
