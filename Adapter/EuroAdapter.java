d3da65f5da02d38aeb2e28b53ba8e75cb2fe3ebb
beccabanks <rebeccabanks916@gmail.com>
2025-11-24 16:11:28 -0500

    Update EuroAdapter.java

--- Adapter/EuroAdapter.java
+++ Adapter/EuroAdapter.java
@@ -1,7 +1,25 @@
 package Adapter;
 
-public class EuroAdapter {
+public class EuroAdapter implements Currency {
 
-	
-	
+    private final double rateFromUsd; //1 USD = 0.92 EUR
+
+    public EuroAdapter(double rateFromUsd) {
+        this.rateFromUsd = rateFromUsd;
+    }
+
+    @Override
+    public double fromUsd(double amountInUsd) {
+        return amountInUsd * rateFromUsd;
+    }
+
+    @Override
+    public String getCode() {
+        return "EUR";
+    }
+
+    @Override
+    public String getSymbol() {
+        return "€";
+    }
 }
