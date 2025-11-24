f410f331c911f68af8811b80a2d80ff2a819dbf7
beccabanks <rebeccabanks916@gmail.com>
2025-11-24 16:11:14 -0500

    Update Currency.java

--- Adapter/Currency.java
+++ Adapter/Currency.java
@@ -1,5 +1,21 @@
 package Adapter;
 
-public class Currency {
+public interface Currency {
+    
+    // Convert an amount in USD to this currency.
+    double fromUsd(double amountInUsd);
 
+    
+    //Short currency code, e.g., "USD", "EUR".
+    String getCode();
+
+    
+    //Symbol to display, e.g., "$", "€".
+    String getSymbol();
+
+    
+    //Helper to format a USD amount directly in this currency.
+    default String formatFromUsd(double amountInUsd) {
+        return String.format("%s%.2f", getSymbol(), fromUsd(amountInUsd));
+    }
 }
