fb076771a6e7d31eafb85d751d5f48afeaf4d540
beccabanks <rebeccabanks916@gmail.com>
2025-11-24 16:16:05 -0500

    Update UserService.java

--- Service/UserService.java
+++ Service/UserService.java
@@ -1,5 +1,69 @@
 package Service;
 
-public class UserService {
+import Model.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
+import java.util.List;
+
+public class UserService {
+    private ObservableList<User> users;
+
+    public UserService() {
+        this.users = FXCollections.observableArrayList();
+    }
+
+    // Create a new user
+    public User createUser(String username, String password, String currency, String email) {
+        User user = new User(username, password, currency, email);
+        users.add(user);
+        return user;
+    }
+
+    // Get all users
+    public List<User> getAllUsers() {
+        return users;
+    }
+
+    // Get user by ID 
+    public User getUserByID(String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    // Update user details
+    public boolean updateUser(String username, String password, String currency, String email) {
+        User user = getUserByID(username);
+        if (user != null) {
+            user.setPassword(password);
+            user.setCurrency(currency);
+            user.setEmail(email);
+            return true;
+        }
+        return false;
+    }
+
+    // Delete user
+    public boolean deleteUser(String username) {
+        User user = getUserByID(username);
+        if (user != null) {
+            users.remove(user);
+            return true;
+        }
+        return false;
+    }
+
+    // Change password
+    public boolean changePassword(String username, String oldPassword, String newPassword) {
+        User user = getUserByID(username);
+        if (user != null && user.getPassword().equals(oldPassword)) {
+            user.setPassword(newPassword);
+            return true;
+        }
+        return false;
+    }
 }
