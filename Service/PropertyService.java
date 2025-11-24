0d2386409b297deb5c3d543c9fe4e2b05ab59ee7
beccabanks <rebeccabanks916@gmail.com>
2025-11-24 16:15:52 -0500

    Update PropertyService.java

--- Service/PropertyService.java
+++ Service/PropertyService.java
@@ -1,5 +1,51 @@
 package Service;
 
+import Model.Property;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PropertyService {
 
+    private List<Property> properties = new ArrayList<>();
+
+    /*
+     * Adds a new property to the system.
+     */
+    public void addProperty(Property property) {
+        properties.add(property);
+    }
+
+    /*
+     * Returns a list of all properties.
+     */
+    public List<Property> getAllProperties() {
+        return properties;
+    }
+
+    /*
+     * Returns a list of available properties.
+     */
+    public List<Property> getAvailableProperties() {
+        List<Property> availableProperties = new ArrayList<>();
+        for (Property property : properties) {
+            if (property.isAvailable()) {
+                availableProperties.add(property);
+            }
+        }
+        return availableProperties;
+    }
+
+    /*
+     * Searches for properties by name that are available.
+     */
+    public List<Property> searchAvailableProperties(String query) {
+        List<Property> results = new ArrayList<>();
+        for (Property p : properties) {
+            if (p.isAvailable() &&
+                    p.getName().toLowerCase().contains(query.toLowerCase())) {
+                results.add(p);
+            }
+        }
+        return results;
+    }
 }
